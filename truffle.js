1aa0c749dd40b74c2f76a9fb5bf188f57d11a437
Aler Denisov <aler.zampillo@gmail.com>
2018-02-22 15:18:41 +0300

    simplify truffle config

--- truffle.js
+++ truffle.js
@@ -1,40 +1,33 @@
-require('dotenv').config();
-const Web3 = require("web3");
-const web3 = new Web3();
-const WalletProvider = require("truffle-wallet-provider");
-const Wallet = require('ethereumjs-wallet');
+require('dotenv').config()
+const Web3 = require('web3')
+const web3 = new Web3()
 
-var mainNetPrivateKey = new Buffer(process.env["MAINNET_PRIVATE_KEY"], "hex")
-var mainNetWallet = Wallet.fromPrivateKey(mainNetPrivateKey);
-var mainNetProvider = new WalletProvider(mainNetWallet, "https://mainnet.infura.io/");
+let rinkebyProvider, mainnetProvider
+const HDWalletProvider = require('truffle-hdwallet-provider-privkey')
 
-var ropstenPrivateKey = new Buffer(process.env["ROPSTEN_PRIVATE_KEY"], "hex")
-var ropstenWallet = Wallet.fromPrivateKey(ropstenPrivateKey);
-var ropstenProvider = new WalletProvider(ropstenWallet, "https://ropsten.infura.io/");
-
+if (!process.env.SOLIDITY_COVERAGE && process.env.PRIVATE_KEY) {
+  rinkebyProvider = new HDWalletProvider(process.env.PRIVATE_KEY, 'https://rinkeby.infura.io')
+  mainnetProvider = new HDWalletProvider(process.env.PRIVATE_KEY, 'https://rinkeby.infura.io')
+}
 
 module.exports = {
   networks: {
-    development: {
-      host: "localhost",
-      port: 9545,
-      network_id: "*" // Match any network id
+    testrpc: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*' // Match any network id
     },
-    ropsten: {
-      provider: ropstenProvider,
-      // You can get the current gasLimit by running
-      // truffle deploy --network ropsten
-      // truffle(ropsten)> web3.eth.getBlock("pending", (error, result) =>
-      //   console.log(result.gasLimit))
+    rinkeby: {
+      provider: rinkebyProvider,
       gas: 4600000,
-      gasPrice: web3.toWei("20", "gwei"),
-      network_id: "3",
+      gasPrice: web3.toWei('20', 'gwei'),
+      network_id: '3'
     },
     mainnet: {
-      provider: mainNetProvider,
+      provider: mainnetProvider,
       gas: 4600000,
-      gasPrice: web3.toWei("20", "gwei"),
-      network_id: "1",
+      gasPrice: web3.toWei('20', 'gwei'),
+      network_id: '1'
     }
   }
-};
+}
