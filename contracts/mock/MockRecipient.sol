2c6ff8cef5782ecd981784292bd445068635385a
Aler Denisov <aler.zampillo@gmail.com>
2018-02-22 16:11:18 +0300

    recipient mock contract

---
+++ contracts/mock/MockRecipient.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.18;
+
+import "../TokenRecipient.sol";
+
+contract MockRecipient is TokenRecipient {
+  address public allowedToken;
+
+  function MockRecipient(address _token) public {
+    allowedToken = _token;
+  }
+
+  function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public {
+    require(_token == allowedToken);
+  }
+}
