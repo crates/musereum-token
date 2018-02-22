2359eff2be81468b87176504cd28cfc0eb4431ce
Aler Denisov <aler.zampillo@gmail.com>
2018-02-22 16:11:05 +0300

    dependencies to work with es7 modules

--- truffle.js
+++ truffle.js
@@ -1,3 +1,11 @@
+require('babel-polyfill')
+require('babel-register')({
+  // Ignore everything in node_modules except node_modules/zeppelin-solidity.
+  presets: ['es2015'],
+  plugins: ['syntax-async-functions', 'transform-regenerator'],
+  ignore: /node_modules\/(?!zeppelin-solidity)/
+})
+
 require('dotenv').config()
 const Web3 = require('web3')
 const web3 = new Web3()
