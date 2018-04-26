c303ba6bfdcbb0e9fd6e8b5daaa0431c178ab575
Aler Denisov <aler.zampillo@gmail.com>
2018-04-26 16:59:07 +0300

    lint code

--- utils/index.js
+++ utils/index.js
@@ -35,7 +35,7 @@ export const hexToBytes = (hexString) => {
 
 // https://github.com/OpenZeppelin/zeppelin-solidity/blob/dd1fd0002ad6b72c917db9e1fadb2938df96c398/test/helpers/latestTime.js#L1
 export const latestTime = () => {
-  return web3.eth.getBlock('latest').timestamp;
+  return web3.eth.getBlock('latest').timestamp
 }
 
 // https://github.com/OpenZeppelin/zeppelin-solidity/blob/dd1fd0002ad6b72c917db9e1fadb2938df96c398/test/helpers/increaseTime.js#L3
@@ -47,14 +47,14 @@ export const increaseTime = (duration) => {
       jsonrpc: '2.0',
       method: 'evm_increaseTime',
       params: [duration],
-      id: id,
+      id: id
     }, err1 => {
       if (err1) return reject(err1)
 
       web3.currentProvider.sendAsync({
         jsonrpc: '2.0',
         method: 'evm_mine',
-        id: id+1,
+        id: id + 1
       }, (err2, res) => {
         return err2 ? reject(err2) : resolve(res)
       })
@@ -64,18 +64,18 @@ export const increaseTime = (duration) => {
 
 // https://github.com/OpenZeppelin/zeppelin-solidity/blob/dd1fd0002ad6b72c917db9e1fadb2938df96c398/test/helpers/increaseTime.js#L27
 export const increaseTimeTo = (target) => {
-  let now = latestTime();
-  if (target < now) throw Error(`Cannot increase current time(${now}) to a moment in the past(${target})`);
-  let diff = target - now;
-  return increaseTime(diff);
+  let now = latestTime()
+  if (target < now) throw Error(`Cannot increase current time(${now}) to a moment in the past(${target})`)
+  let diff = target - now
+  return increaseTime(diff)
 }
 
 // https://github.com/OpenZeppelin/zeppelin-solidity/blob/c05918c3cc10d9394fd5fb95deb04036204ac896/test/helpers/increaseTime.js#L41
 export const duration = {
-  seconds: function (val) { return val; },
-  minutes: function (val) { return val * this.seconds(60); },
-  hours: function (val) { return val * this.minutes(60); },
-  days: function (val) { return val * this.hours(24); },
-  weeks: function (val) { return val * this.days(7); },
-  years: function (val) { return val * this.days(365); },
-};
+  seconds: function (val) { return val },
+  minutes: function (val) { return val * this.seconds(60) },
+  hours: function (val) { return val * this.minutes(60) },
+  days: function (val) { return val * this.hours(24) },
+  weeks: function (val) { return val * this.days(7) },
+  years: function (val) { return val * this.days(365) }
+}
