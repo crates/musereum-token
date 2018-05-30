fab35da8ed4063e2c502ea8b144f3d60adfbb56b
Aler Denisov <aler.zampillo@gmail.com>
2018-05-30 21:25:23 +0300

    change %

--- contracts/music/ToTheMoonDistribution.sol
+++ contracts/music/ToTheMoonDistribution.sol
@@ -4,21 +4,24 @@ import './DistributeEther.sol';
 
 contract ToTheMoonDistribution is DistributeEther {
   address[] private recipients = [
-    // Artists
-    address(0x0),
     // McAfee
+    address(0x0),
+    // Artists
     address(0x0),
     // Marketing
     address(0x0),
     // Musereum
-    address(0x0)
+    address(0x0),
+    // Dev
+    address(0x0),
   ];
 
   uint[] private partions = [
-    5000,
-    2500,
-    800,
-    1200
+    5000, // mcafee
+    2500, // artists
+    800,  // marketing
+    1200, // musereum
+    500   // dev
   ];
 
   constructor() DistributionEther(recipients, partions) public {}
