03095a200610e6c482bb43178a5fe2ab7f493423
Aler Denisov <aler.zampillo@gmail.com>
2018-05-30 07:49:39 +0300

    dependencies

--- truffle.js
+++ truffle.js
@@ -14,13 +14,27 @@ module.exports = {
     testrpc: {
       host: 'localhost',
       port: 8545,
+      gasPrice: 0x0,
       network_id: '*' // Match any network id
+    },
+    make: {
+      gasPrice: 1,
+      network_id: '*',
+      host: 'localhost',
+      port: 8545,
+      from: '0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1'     // Use the address we derived
     },
     rinkeby: {
       provider: () => new HDWalletProvider(process.env.PRIVATE_KEY, 'https://rinkeby.infura.io'),
       gas: 4600000,
       gasPrice: 5e9,
       network_id: '3'
+    },
+    mainnet: {
+      provider: () => new HDWalletProvider(process.env.PRIVATE_KEY, 'http://localhost:8545'),
+      gas: 4000000,
+      gasPrice: 8e9,
+      network_id: 1
     }
   }
 }
