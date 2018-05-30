e6fb429ea62c261435a7afab154d073f8e3814f0
Aler Denisov <aler.zampillo@gmail.com>
2018-05-30 07:50:12 +0300

    Music ico with ETH

--- contracts/music/MusicICO.sol
+++ contracts/music/MusicICO.sol
@@ -1,144 +1,140 @@
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.24;
 
 import "zeppelin-solidity/contracts/math/SafeMath.sol";
 import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 import "zeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
-import "./../token/ERC223Recipient.sol";
 
-contract MusicICO is ERC223Recipient, Ownable {
-  using SafeMath for uint;
+contract MusicICO is Ownable {
+    using SafeMath for uint;
 
-  string public constant name = "Music Token ICO";
+    string public constant name = "Music Token ICO";
 
-  // TODO: change it
-  uint public constant dust = 180 finney;
-  uint public constant tokensCap = 100000 * (10 ** uint256(18));
-  uint public constant priceUsd = 10 ether; // 10 cent
+    // TODO: change it
+    uint public constant dust = 180 finney;
+    uint public constant tokensCap = 100000 * (10 ** uint256(18));
+    uint public constant priceUsd = 50000; // 1 ETH -> 500$ 
 
-  address public beneficiary;
-  MintableToken public musicToken;
-  MintableToken public copyrightToken;
-  StandardToken public musereumToken;
-  uint public startTime;
-  uint public endTime;
+    address public beneficiary;
+    MintableToken public musicToken;
+    MintableToken public copyrightToken;
+    uint public startTime;
+    uint public endTime;
 
-  uint public investorCount;
-  uint public tokenReceived;
-  uint public tokenCollected;
-  mapping (address => uint) public tokenDeposits;
-  
+    uint public investorCount;
+    uint public etherReceived;
+    uint public etherCollected;
+    mapping (address => uint) public etherDeposits;
+    
 
-  event Received(address _from, uint _tokens);
-  event Collected(address _from, uint _tokens);
-  event Redeemed(address _from, uint _tokens);
-  event Finalized(address _newOwner);
+    event Received(address _from, uint _value);
+    event Collected(address _from, uint _value);
+    event Redeemed(address _from, uint _tokens);
+    event Finalized(address _newOwner);
 
-  modifier only_during_period {require(block.timestamp >= startTime && block.timestamp < endTime); _;}
-  modifier only_after_period { require(block.timestamp >= endTime); _; }
-  modifier is_not_dust(uint _value) {require(_value >= dust); _;}
+    modifier only_during_period {require(block.timestamp >= startTime && block.timestamp < endTime); _;}
+    modifier only_after_period { require(block.timestamp >= endTime); _; }
+    modifier is_not_dust(uint _value) {require(_value >= dust); _;}
 
-  function MusicICO(
-    address _beneficiary,
+    constructor(
+        address _beneficiary,
 
-    address _musicToken,
-    address _copyrightToken,
-    address _musereumToken,
+        address _musicToken,
+        address _copyrightToken,
 
-    uint _startTime,
-    uint _endTime
-  ) public {
-    beneficiary = _beneficiary;
-
-    musicToken = MintableToken(_musicToken);
-    copyrightToken = MintableToken(_copyrightToken);
-    musereumToken = StandardToken(_musereumToken);
-
-    startTime = _startTime;
-    endTime = _endTime;
-  }
-
-  /**
-    * Fallback function
-    * Preventing someone to send ether to this contract
-    */
-  function () public payable {
-    require(false);
-  }
-
-  function tokenFallback(address _from, uint256 _value, bytes _extraData) 
-    public only_during_period is_not_dust(_value)
-  {
-    _extraData;
-    // require(msg.sender == address(musereumToken));
-
-    uint bonus = calculateBonusPercentage(_value);
-    uint tokens = _value;
-    if (bonus > 0) {
-      // Use 100 ether and ether because we don't want loose percision
-      tokens = tokens.add(tokens.mul(bonus).div(100 ether)); // tokens + tokens * 0.(bonus percentage)
+        uint _startTime,
+        uint _endTime
+    ) public {
+        beneficiary = _beneficiary;
+        musicToken = MintableToken(_musicToken);
+        copyrightToken = MintableToken(_copyrightToken);
+        startTime = _startTime;
+        endTime = _endTime;
     }
 
-    if (tokenDeposits[_from] == 0) {
-      investorCount = investorCount.add(1);
+    /**
+      * Fallback function
+      * Preventing someone to send ether to this contract
+      */
+    function() public payable {
+        buyTokens(msg.sender);
     }
-    tokenReceived = tokenReceived.add(_value);
-    tokenCollected = tokenCollected.add(tokens);
-    tokenDeposits[_from] = tokenDeposits[_from].add(tokens);
 
-    Received(_from, _value);
-    Collected(_from, tokens);
-  }
+    function buyTokens(address buyer) public payable only_during_period is_not_dust(msg.value) {
+        uint bonus = calculateBonusPercentage(msg.value);
+        uint value = msg.value;
+        if (bonus > 0) {
+            // Use 100 ether and ether because we don't want loose percision
+            value = value.add(value.mul(bonus).div(100 ether)); // value + value * 0.(bonus percentage)
+        }
 
-  function calculateBonusPercentage(uint tokens) 
-    public pure returns (uint bonus) 
-  {
-    uint tokensUsd = tokens.div(100 ether).mul(priceUsd);
-    bonus = 0;
+        if (etherDeposits[buyer] == 0) {
+            investorCount = investorCount.add(1);
+        }
+        etherReceived = etherReceived.add(msg.value);
+        etherCollected = etherCollected.add(value);
+        etherDeposits[buyer] = etherDeposits[buyer].add(value);
 
-    // from $500 + 10%
-    if (tokensUsd >= 500 ether && tokensUsd < 1000 ether) {
-      bonus = 10 ether;
-    // from $1000 + 20%
-    } else if (tokensUsd >= 1000 ether && tokensUsd < 3000 ether) {
-      bonus = 20 ether;
-    // from $3000 + 30%
-    } else if (tokensUsd >= 3000 ether && tokensUsd < 5000 ether) {
-      bonus = 30 ether;
-    // from $5000 + 40%
-    } else if (tokensUsd >= 5000 ether) {
-      bonus = 40 ether;
-    } 
-    return bonus;
-  }
+        emit Received(buyer, msg.value);
+        emit Collected(buyer, value);
 
-  function admin_redeem(address _to) public only_after_period onlyOwner {
-    redeem(_to);
-  }
+        require(beneficiary.call.value(msg.value)());
+    }
 
-  function user_redeem() public only_after_period {
-    redeem(msg.sender);
-  }
+    function calculateBonusPercentage(uint value) 
+        public pure returns (uint bonus) 
+    {
+        uint valueUsd = value.mul(priceUsd);
 
-  function redeem(address _from) internal {
-    require(tokenDeposits[_from] > 0);
+        // 5000$ -> 40% bonus
+        if (valueUsd >= 500000 ether) {
+            return 40 ether;
+        }
 
-    uint percentage = tokenDeposits[_from].mul(100 ether).div(tokenCollected);
-    uint tokens = tokensCap.mul(percentage).div(100 ether); // tokenCollected * 0.(percentage)
+        // 3000$ -> 30% bonus
+        if (valueUsd >= 300000 ether) {
+            return 30 ether;
+        }
 
-    musicToken.mint(_from, tokens);
-    copyrightToken.mint(_from, tokens);
-    tokenDeposits[_from] = 0;
+        // 1000$ -> 20% bonus
+        if (valueUsd >= 100000 ether) { 
+            return 20 ether;
+        }
 
-    Redeemed(_from, tokens);
-  }
+        // 500$ -> 10% bonus
+        if (valueUsd >= 50000 ether) {
+            return 10 ether;
+        }
+        
+        return 0;
+    }
 
-  function finalizeIt(address _newOwner) public only_after_period onlyOwner {
-    require(_newOwner != 0x0);
+    function admin_redeem(address _to) public only_after_period onlyOwner {
+        redeem(_to);
+    }
 
-    musereumToken.transfer(beneficiary, musereumToken.balanceOf(this));
-    musicToken.transferOwnership(_newOwner);
-    copyrightToken.transferOwnership(_newOwner);
-    Finalized(_newOwner);
-  }
+    function user_redeem() public only_after_period {
+        redeem(msg.sender);
+    }
+
+    function redeem(address _from) internal {
+        require(etherDeposits[_from] > 0);
+
+        uint percentage = etherDeposits[_from].mul(100 ether).div(etherCollected);
+        uint tokens = tokensCap.mul(percentage).div(100 ether); // etherCollected * 0.(percentage)
+
+        musicToken.mint(_from, tokens);
+        copyrightToken.mint(_from, tokens);
+        etherDeposits[_from] = 0;
+
+        emit Redeemed(_from, tokens);
+    }
+
+    function finalizeIt(address _newOwner) public only_after_period onlyOwner {
+        require(_newOwner != 0x0);
+        musicToken.transferOwnership(_newOwner);
+        copyrightToken.transferOwnership(_newOwner);
+
+        emit Finalized(_newOwner);
+    }
 }
 
